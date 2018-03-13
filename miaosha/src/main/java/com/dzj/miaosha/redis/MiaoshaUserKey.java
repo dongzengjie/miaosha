0a9961851ee7eee1c435a32b61ef26a8ef68e074
dongzengjie <2857877087@qq.com>
2018-03-13 14:30:00 +0800

    login ...

--- miaosha/src/main/java/com/dzj/miaosha/redis/MiaoshaUserKey.java
+++ miaosha/src/main/java/com/dzj/miaosha/redis/MiaoshaUserKey.java
@@ -1,5 +1,14 @@
 package com.dzj.miaosha.redis;
 
-public class MiaoshaUserKey {
+public class MiaoshaUserKey extends BassPrefix {
+
+	public static final int TOKEN_EXPIRE=3600 * 24 ;
+	
+	private MiaoshaUserKey(int expireSeconds, String prefix) {
+		super(expireSeconds, prefix);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static MiaoshaUserKey token =new MiaoshaUserKey(TOKEN_EXPIRE, "tk");
 
 }
--- miaosha/src/main/java/com/dzj/miaosha/vo/LoginVo.java
+++ miaosha/src/main/java/com/dzj/miaosha/vo/LoginVo.java
@@ -1,5 +1,38 @@
 package com.dzj.miaosha.vo;
 
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+/**
+ * 校验用户登陆
+ * @author DZJ
+ *
+ */
 public class LoginVo {
 
+	@NotNull
+	private String mobile;
+	
+	@NotNull
+	@Length(min=11)
+	private String password;
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 }
